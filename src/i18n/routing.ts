920310054b27420d0ebda50389ba85d03e60cb95
moocads <info@moocads.com>
2025-07-06 14:04:33 -0400

    Support both Simplified and Traditional Chinese

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'zh-CN'],
+  locales: ['en', 'zh-CN', 'zh-TC'],
   defaultLocale: 'en',
   pathnames: {
     '/': '/',
